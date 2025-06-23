df3dedbc9055716abc271fe4edcad149d1bbc2bb
Andrey Smetanin <cmutyxa@gmail.com>
2025-06-23 17:19:03 +0300

    start work

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom"
+
 export default function Header() {
     return (
         <header>
-            Основные хуки практика           
+           <Link to="/" className="mainPageLink">Основные хуки практика</Link>
         </header>
     )
 }
